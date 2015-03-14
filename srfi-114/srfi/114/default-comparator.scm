683889d21a760ad67413396f5392d47c659d8e42
ilammy <a.lozovsky@gmail.com>
2015-03-14 15:02:28 +0200

    Added extension registry for the default comparator
    Other system libraries can call register-default-comparator! to add
    their type-specific comparator to the global registry. The intended
    usage of this procedure is limited to the phase when libraries are
    loaded. As Scheme does not have a standard way to unload a library,
    no means are provided to unregister a comparator.

--- srfi-114/srfi/114/default-comparator.scm
+++ srfi-114/srfi/114/default-comparator.scm
@@ -3,6 +3,34 @@
 ;; 3-clause BSD license: http://github.com/ilammy/srfi-114/blob/master/LICENSE
 
 
+;; Default comparator extension registry ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; This facility is an implementation-private interface for extending
+;; the default comparator. It is indended to be used by implementation-
+;; specific libraries (i.e., system libraries) to register their types
+;; within the default comparator of SRFI 114.
+;;
+;; It should *not* be used by user-level libraries.
+;;
+;; Also, it is not thread-safe so add necessary synchronization to all
+;; exported procedures if you allow to load libraries concurrently.
+
+(define *comparators* '())
+(define *total-comparators* 0)
+
+(define (register-default-comparator! comparator)
+  (set! *comparators* (cons comparator *comparators*))
+  (set! *total-comparators* (+ 1 *total-comparators*)))
+
+(define (query-default-comparator object)
+  (let loop ((tested 0) (comparators *comparators*))
+    (if (null? comparators)
+        (values #f #f)
+        (if (comparator-test-type (car comparators) object)
+            (values (- *total-comparators* tested) (car comparators))
+            (loop (+ 1 tested) (cdr comparators))))))
+
+
 ;; Default comparison and hashing ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define fallback-comparator
@@ -12,8 +40,10 @@
     hash))
 
 ; This procedure defines the default ordering of objects by their types. Objects
-; of known primitive types have strict order between them. All other objects are
-; treated as equal. Primitive objects are less than any non-primitive object.
+; of known primitive types have strict order between them. Objects of registered
+; extension types have strict order between them. All other objects are treated
+; as equal. Primitive objects are less than any non-primitive object. Registered
+; extension objects are less than any non-primitive and non-extension object.
 (define (choose-default-comparator obj)
   (cond ((null? obj)       (values 0 null-comparator))
         ((pair? obj)       (values 1 pair-comparator))
@@ -24,7 +54,11 @@
         ((number? obj)     (values 6 number-comparator))
         ((vector? obj)     (values 7 vector-comparator))
         ((bytevector? obj) (values 8 bytevector-comparator))
-        (else              (values 9 fallback-comparator))))
+        (else
+          (let-values (((offset comparator) (query-default-comparator obj)))
+            (if offset
+                (values (+ 8 offset) comparator)
+                (values +inf.0 fallback-comparator))))))
 
 (define (default-equality obj1 obj2)
   (let-values (((obj1-order obj1-comparator) (choose-default-comparator obj1))
